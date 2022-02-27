8f723d9faf7688dca16234ad1cc52cca532368cf
xj <83954439@qq.com>
2022-02-27 13:04:32 +0800

    Test

---
+++ src/main/java/com/xiaji/test1/Test1Application.java
@@ -0,0 +1,14 @@
+package com.xiaji.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test1Application.class, args);
+        System.out.println("------------------hello World---------------------");
+    }
+
+}
